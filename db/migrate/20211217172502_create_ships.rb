313a2aea0441e106d193fe92b7801b50930fb19b
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-12-17 13:31:01 -0500

    building out xwing data model

---
+++ db/migrate/20211217172502_create_ships.rb
@@ -0,0 +1,13 @@
+class CreateShips < ActiveRecord::Migration[7.0]
+  def change
+    create_table :ships do |t|
+      t.string :name
+      t.integer :attack
+      t.integer :hull
+      t.integer :shields
+      t.integer :agility
+
+      t.timestamps
+    end
+  end
+end
