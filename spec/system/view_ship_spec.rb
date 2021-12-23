605aae68ecf049c9cb975774626c411360bbca7e
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-12-23 15:40:31 -0500

    test no longer pending

--- spec/system/view_ship_spec.rb
+++ spec/system/view_ship_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe 'viewing a ship', type: :system do
-  it 'allows a user to view a ship', pending: true do
+  it 'allows a user to view a ship' do
     visit ships_path
     click_on('X-Wing')
     expect(page).to have_content('X-Wing')
