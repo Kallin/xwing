b49d4df59fbf3d6beb71e7bae6afed4b40856533
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-12-26 16:13:12 -0500

    eliminating instance variable from helper (rubocop)

--- app/helpers/collections_helper.rb
+++ app/helpers/collections_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 module CollectionsHelper
-  def ship_count(ship)
-    ship_count_for_ship = @ship_counts.find { |ship_count| ship_count.ship_id == ship.id }
+  def ship_count(ship, ship_counts)
+    ship_count_for_ship = ship_counts.find { |ship_count| ship_count.ship_id == ship.id }
     ship_count_for_ship ? ship_count_for_ship.count : 0
   end
 end
