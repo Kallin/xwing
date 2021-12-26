fa0740f86cdcf0a6970d8c3c380947287afcad49
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-12-26 16:33:27 -0500

    translator cleanup for method length (rubocop)

--- lib/catalog/translator.rb
+++ lib/catalog/translator.rb
@@ -7,22 +7,32 @@ module Catalog
     def translate_pilots
       @ships = []
       @pilots = []
-      @input_pilots.each do |input_ship|
-        @ships << {
-          name: input_ship['name'],
-          faction: input_ship['faction'],
-          agility: find_stat(input_ship, 'agility'),
-          hull: find_stat(input_ship, 'hull'),
-          shields: find_stat(input_ship, 'shields'),
-          energy: find_stat(input_ship, 'energy')
+      @input_pilots.each { |input_pilot| extract_ships_and_pilots(input_pilot) }
+    end
+
+    def extract_ships_and_pilots(input_ship)
+      extract_ships(input_ship)
+      extract_pilots(input_ship)
+    end
+
+    def extract_pilots(input_ship)
+      input_ship['pilots'].each do |actual_input_pilot|
+        @pilots << {
+          name: actual_input_pilot['name'],
+          ship: input_ship['name']
         }
-        input_ship['pilots'].each do |actual_input_pilot|
-          @pilots << {
-            name: actual_input_pilot['name'],
-            ship: input_ship['name']
-          }
-        end
       end
+    end
+
+    def extract_ships(input_ship)
+      @ships << {
+        name: input_ship['name'],
+        faction: input_ship['faction'],
+        agility: find_stat(input_ship, 'agility'),
+        hull: find_stat(input_ship, 'hull'),
+        shields: find_stat(input_ship, 'shields'),
+        energy: find_stat(input_ship, 'energy')
+      }
     end
 
     def find_stat(input_ship, stat_name)
