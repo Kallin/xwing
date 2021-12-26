0b6d4313b365e02b5ef546b22f1f4fbaffcfcde2
Kallin Nagelberg <kallin.nagelberg@gmail.com>
2021-12-26 16:30:24 -0500

    lots of rubocop cleanupfor lib_loader

--- lib/lib_loader.rb
+++ lib/lib_loader.rb
@@ -3,20 +3,28 @@
 class LibLoader
   def load_ships
     input_ships = load_from_input_file
-    mapped_ships = input_ships.map do |ship|
+    mapped_ships = extract_relevant_attributes(input_ships)
+    Ship.create!(mapped_ships)
+
+    add_factions_and_actions(input_ships)
+  end
+
+  def add_factions_and_actions(input_ships)
+    Ship.find_each do |ship|
+      input_ship = input_ships.find { |the_ship| the_ship['name'] == ship.name }
+      input_ship['actions'].each { |input_action| ship.actions << (Action.find_by name: input_action) }
+      input_ship['factions'].each { |input_faction| ship.factions << (Faction.find_by name: input_faction) }
+    end
+  end
+
+  def extract_relevant_attributes(input_ships)
+    input_ships.map do |ship|
       {
         name: ship['name'],
         attack: ship['attack'],
         agility: ship['agility'],
         hull: ship['hull']
       }
-    end
-    Ship.insert_all(mapped_ships)
-
-    Ship.find_each do |ship|
-      input_ship = input_ships.find { |the_ship| the_ship['name'] == ship.name }
-      input_ship['actions'].each { |input_action| ship.actions << (Action.find_by name: input_action) }
-      input_ship['factions'].each { |input_faction| ship.factions << (Faction.find_by name: input_faction) }
     end
   end
 
@@ -33,7 +41,7 @@ class LibLoader
   def insert_all_entities(path, klazz)
     entities = JSON.parse(File.read(path))
     entities.map { |f| { name: f } }
-    klazz.insert_all(entities.map { |f| { name: f } })
+    klazz.create!(entities.map { |f| { name: f } })
   end
 
   def build_lib_file_path(file_name)
